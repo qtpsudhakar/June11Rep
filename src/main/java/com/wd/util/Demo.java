999aae2521169906cf2b595fe84b2558d45101d3
qtpsudhakar <sudhakar@qtpsudhakar.com>
2018-07-27 09:05:07 +0530

    Update Demo.java
    remote change

--- src/main/java/com/wd/util/Demo.java
+++ src/main/java/com/wd/util/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
 		System.out.println("this is demo");
 		System.out.println("this is demo1");
 		System.out.println("this is demo2");
+		System.out.println("this is a change from remote");
 	}
 
 }
